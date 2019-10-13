e5271b23a80c1806306ac8f88f702549f0b79797
manuel <manunoly@gmail.com>
2019-10-13 14:01:35 -0500

    cargando los terminos desde la BD

--- src/app/login/terminos/terminos.page.ts
+++ src/app/login/terminos/terminos.page.ts
@@ -1,3 +1,4 @@
+import { DbService } from './../../servicios/db.service';
 import { ModalController } from '@ionic/angular';
 import { UtilService } from './../../servicios/util.service';
 import { Component, OnInit } from '@angular/core';
@@ -8,23 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./terminos.page.scss'],
 })
 export class TerminosPage implements OnInit {
-  terminos = `
-  <ul>
-  <li> bla bla bla 4to nivel. PHD / Master</li>
-  <li>Experiencia catedr&aacute;tica</li>
-  <li>Dominio de la Materia</li>
-  <li>Clases Personalizadas</li>
-  <li>Clase a domicilio / Lugar de preferencia</li>
-  <li>Elecci&oacute;n de horario a su gusto</li>
-  <li>Servicio VIP</li>
-  <li>Surprise Gift</li>
-  <li>Las horas de tu Combo pueden ser utilizadas en Proyectos o Clases</li>
-  </ul>`;
+  terminos;
 
 
-  constructor(private modalController: ModalController) { }
+  constructor(private modalController: ModalController, private db: DbService) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.terminos = await this.db.get('reglamento');
   }
 
   back(ok?) {
