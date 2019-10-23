1dfdae592e707763833a3d46d687de5227a9425e
manuel <manunoly@gmail.com>
2019-10-23 08:35:44 -0500

    pongo el home como homepage para que no salga de la pagina y mejoro detalles del profesor que ya se habian hecho

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,4 @@
-import { AuthService } from './../servicios/auth.service';
 import { Component, OnInit } from '@angular/core';
-import { AlertController } from '@ionic/angular';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -9,19 +6,11 @@ import { Router } from '@angular/router';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
-  user;
 
-  constructor(public alertController: AlertController, private router: Router, private auth: AuthService) { }
+  constructor() { }
 
   async ngOnInit() {
-    this.auth.currentUser.subscribe(user => {
-      console.log(user);
-      this.user = user;
-    })
-  }
 
-  ir(url) {
-    this.router.navigateByUrl(url);
   }
 
 }
