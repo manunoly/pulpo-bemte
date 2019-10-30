0203edde0e685a711928c8010bd22f7dc46cc97d
manuel <manunoly@gmail.com>
2019-10-30 14:05:56 -0500

    navegando desde el inicio para no salir de la apk

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
+import { AuthService } from './../servicios/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthService, private router: Router) { }
 
   async ngOnInit() {
-
   }
 
+  goTo() {
+    const user = this.auth.getUserData();
+    if (user && user['tipo'] == 'Profesor')
+      this.router.navigateByUrl('inicio-profesor');
+    else if (user && user['tipo'] == 'Alumno')
+      this.router.navigateByUrl('inicio');
+    else
+      this.router.navigateByUrl('login');
+
+
+  }
 }
