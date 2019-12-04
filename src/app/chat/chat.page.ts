74417056195b5b3f98947ae35788947ff63d9a06
manuel <manunoly@gmail.com>
2019-12-04 09:52:16 -0500

    chat para las tareas

--- src/app/chat/chat.page.ts
+++ src/app/chat/chat.page.ts
@@ -1,6 +1,6 @@
 import { DbService } from './../servicios/db.service';
 import { UtilService } from './../servicios/util.service';
-import { ModalController } from '@ionic/angular';
+import { ModalController, AlertController } from '@ionic/angular';
 import { AuthService } from './../servicios/auth.service';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { switchMap } from 'rxjs/operators';
@@ -24,11 +24,12 @@ export class ChatPage implements OnInit {
   datosMostrar;
   tipo;
 
-  constructor(private db: DbService, public util: UtilService, public auth: AuthService, private modalController: ModalController) { }
+  constructor(private alertController: AlertController, private db: DbService, public util: UtilService, public auth: AuthService, private modalController: ModalController) { }
 
   async ngOnInit() {
     console.log('recibo clase', this.clase);
     console.log('recibo tarea', this.tarea);
+    console.log('tipo', this.tipo);
     if (!this.clase && !this.tarea) {
       this.util.showMessage('No hemos podido obtener los datos');
       setTimeout(() => {
@@ -104,6 +105,66 @@ export class ChatPage implements OnInit {
 
   close() {
     this.modalController.dismiss();
+  }
+
+
+  async terminar() {
+    try {
+      this.util.showLoading();
+      const resp = await this.db.post('tarea-terminar', { clase_id: 0, tarea_id: this.tarea.id, user_id: this.user.user_id, cancelar: 0, profesor: 0 });
+      this.util.dismissLoading();
+      if (resp && resp.success) {
+        this.util.showMessage(resp.success);
+        this.close();
+      }
+    } catch (error) {
+      this.util.dismissLoading();
+    }
+  }
+
+  async finalizarTarea() {
+    {
+      let tipo = 'Profesor';
+      if (this.user.tipo == 'Profesor')
+        tipo = 'Estudiante'
+      const alert = await this.alertController.create({
+        header: 'Finalizar la tarea!',
+        message: `Al finalizar la tarea, se finaliza tambien el contacto con el ${tipo}.`,
+        cssClass: 'fondoVerde alertDefault',
+        inputs: [
+          {
+            name: 'Si',
+            type: 'radio',
+            label: 'Si',
+            value: true
+          },
+          {
+            name: 'No',
+            type: 'radio',
+            label: 'No',
+            value: false
+          }],
+        buttons: [
+          {
+            text: 'Atrás',
+            role: 'cancel',
+            handler: (blah) => {
+              console.log('Confirm Cancel: blah');
+            }
+          }, {
+            text: 'Aceptar',
+            handler: (data) => {
+              if (data) {
+                this.terminar();
+              }
+            }
+          }
+        ]
+      });
+
+      await alert.present();
+    }
+
   }
 
 }
