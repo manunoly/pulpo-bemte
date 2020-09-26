6cd3b5d35071eda2f35e853a91602b5ee4fdc4a7
Manuel Almaguer <manu@Manuels-MacBook-Pro.local>
2020-09-26 14:55:12 -0500

    new url server

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   // api_url: 'http://192.168.0.5/index.php/api/'
-  api_url: 'https://pulpo.digital/bemte-backend/public/index.php/api/',
-  photo_url: 'https://pulpo.digital/bemte-backend/public/storage/',
+  api_url: 'https://portal.bemte.ec/index.php/api/',
+  photo_url: 'https://portal.bemte.ec/storage/',
   test: true
 };
 
