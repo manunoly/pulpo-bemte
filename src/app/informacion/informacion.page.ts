00ce7874519a85713f02e27de75f49785a52e200
Manuel Almaguer <manu@Manuels-MacBook-Pro.local>
2020-09-26 14:56:50 -0500

    nueva url para terminos de estudiante

--- src/app/informacion/informacion.page.ts
+++ src/app/informacion/informacion.page.ts
@@ -1,8 +1,10 @@
+import { UtilService } from 'src/app/servicios/util.service';
 import { DbService } from 'src/app/servicios/db.service';
 import { ModalController } from '@ionic/angular';
 import { AuthService } from './../servicios/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { TerminosPage } from '../login/terminos/terminos.page';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 @Component({
   selector: 'app-informacion',
@@ -11,24 +13,28 @@ import { TerminosPage } from '../login/terminos/terminos.page';
 })
 export class InformacionPage implements OnInit {
 
-  constructor(public auth: AuthService, private modalController: ModalController, private db: DbService) { }
+  constructor(public auth: AuthService, private modalController: ModalController, private db: DbService, private util: UtilService,
+    private iab: InAppBrowser) { }
 
   ngOnInit() {
   }
 
   async confirmarTerminos() {
-
-    const modal = await this.modalController.create({
-      component: TerminosPage
-    });
-
-    return await modal.present();
+      this.util.showLoading();
+      const terminos = await this.db.get('terminos');
+  
+      if (!terminos || !terminos.terminosUrl) return;
+      this.iab.create(terminos.terminosUrl, '_system');
+      this.util.dismissLoading();
   }
 
   async openVideo() {
-    const video = await this.db.get('video');
-    console.log(video);
-    window.location.assign('https://youtube.com');
+    this.util.showLoading();
+    const data = await this.db.get('video');
+
+    if (!data || !data.videoUrl) return;
+    this.iab.create(data.videoUrl, '_system');
+    this.util.dismissLoading();
   }
 
 }
